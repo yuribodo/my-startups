c97519c18928a56390a585eb36d3fc9fdb5d4425
yuribodo <yemotalara@gmail.com>
2024-11-02 15:16:46 -0400

    feat: add main text to hero page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,14 @@
 export default function Home() {
   return (
     <>
-      hOME
+      <section className="w-full bg-blue-950 min-h-[530px] pattern flex justify-center items-center flex-col py-10 px-6">
+
+        <h2 className="px-6 uppercase py-3 font-bold text-black bg-green-500">connect with entrepenaurs</h2>
+
+        <h1 className="heading">Discover news Startups <br/> Find the next Oportunity </h1>
+
+        <p className="bg-white px-6 py-1 text-black font-semibold "> show your bussiness to the world</p>
+      </section>
     </>
   );
 }
