bcbe215d0bdf2b0c2f87e52c5535f2cf669df5e9
yuribodo <yemotalara@gmail.com>
2024-11-02 18:18:22 -0400

    feat: add search bar

---
+++ app/components/SearchFormReset.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import Link from "next/link";
+
+export default function SearchFormReset() {
+  const reset = () => {
+    const form = document.querySelector('.search-form') as HTMLFormElement;
+    if (form) form.reset();
+  };
+
+  return (
+    <button 
+      type="reset" 
+      onClick={reset}
+    >
+      <Link href="/" className="search-btn text-white">
+        X
+      </Link>
+    </button>
+  );
+}
