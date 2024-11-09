e4dc4aed0a77d35c3ff242b7296012478beaaa9f
yuribodo <yemotalara@gmail.com>
2024-11-08 20:05:43 -0400

    feat: fetch post information

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,12 +1,21 @@
+import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries'
+import { client } from '@/sanity/lib/client'
 import React from 'react'
+import { notFound } from 'next/navigation'
+
+export const experimental_ppr = true
 
 const page = async ({params} : {params: Promise<{id: string}>}) => {
 
-    const id = (await params).id
+  const id = (await params).id
 
+  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+
+  if(!post) return notFound();
   return (
     <>
-        <h1 className='text-3xl'>This is a startup number {id}</h1>
+        <h1 className='text-3xl'>{post?.title}</h1>
     </>
   )
 }
