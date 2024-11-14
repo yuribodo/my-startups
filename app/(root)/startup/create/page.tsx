590c751f3102971b5700a7a6df03e4f9f5283839
yuribodo <yemotalara@gmail.com>
2024-11-13 21:28:47 -0400

    feat: add text area and input from shadcn

---
+++ app/(root)/startup/create/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex flex-col justify-center items-center'>
+
+        <h1 className='text-3xl p-4 font-bold'>Show Your Startup To The World</h1>
+
+
+        
+
+    </div>
+  )
+}
+
+export default page
