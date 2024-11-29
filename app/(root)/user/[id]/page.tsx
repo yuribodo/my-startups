5069a10823577dc1a76a96c142d40a11eb5197bd
yuribodo <yemotalara@gmail.com>
2024-11-28 22:14:37 -0400

    get user id but not redering the page

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -1,10 +1,19 @@
-'use client';
 
 import React from "react";
+import { auth } from "@/auth";
+import { client } from "@/sanity/lib/client";
+import { AUTHOR_BY_ID_QUERY } from "@/sanity/lib/queries";
 import { motion } from "framer-motion";
+import { notFound } from "next/navigation";
 import { Camera, Linkedin, Twitter, MapPin, Briefcase, StarIcon } from "lucide-react";
 
-const page = () => {
+const page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const id = (await params).id;
+  const session = await auth();
+
+  const user = await client.fetch(AUTHOR_BY_ID_QUERY, { id });
+  if (!user) return notFound();
+
   const userData = {
     name: "João Silva",
     role: "Startup Founder & Innovator",
